22edd3478145e7c0898d015bd36e60436b6e03a1
Andre Ferreira (MEA) <andre.ferreira@dimensiondata.com>
2017-01-19 13:24:10 +0200

    First commit

---
+++ inc/config.h
@@ -0,0 +1,15 @@
+#ifndef _LORAGW_CONFIGURATION_H
+#define _LORAGW_CONFIGURATION_H
+	#define LIBLORAGW_VERSION	"1.7.0"
+	#define CFG_SPI_FTDI	1
+	#define CFG_CHIP_1301	1
+	#define CFG_RADIO_1257	1
+	#define CFG_BAND_868	1
+	#define CFG_BRD_1301REF868	1
+	#define CFG_NET_LORAMAC	1
+	#define DEBUG_AUX	0
+	#define DEBUG_SPI	0
+	#define DEBUG_REG	0
+	#define DEBUG_HAL	0
+	#define DEBUG_GPS	0
+#endif
